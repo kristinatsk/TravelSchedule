068f10ab0da478ea33b880d60439d1ff663609aa
kristinatsk <kristinatsk@gmail.com>
2026-06-18 00:39:26 +0300

    feat: implement NearestStation and Copyrights services

--- TravelSchedule/ContentView.swift
+++ TravelSchedule/ContentView.swift
@@ -1,11 +1,5 @@
-//
-//  ContentView.swift
-//  TravelSchedule
-//
-//  Created by Kristina Kostenko on 15.06.26.
-//
-
 import SwiftUI
+import OpenAPIURLSession
 
 struct ContentView: View {
     var body: some View {
@@ -16,6 +10,56 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .onAppear {
+            testFetchStations()
+            testFetchCopyright()
+        }
+    }
+    
+    func testFetchStations() {
+        Task {
+            do {
+                let client = Client(
+                    serverURL: try Servers.Server1.url(),
+                    transport: URLSessionTransport()
+                )
+                let service = NearestStationsService(
+                    client: client,
+                    apikey: "e0940f60-7b86-40f1-ba94-6a70f7d38166"
+                )
+                print("Fetching stations...")
+                let stations = try await service.getNearestStations(
+                    lat: 59.864177,
+                    lng: 30.319163,
+                    distance: 50
+                )
+                print("Successfully fetched stations: \(stations)")
+            } catch {
+                print("Error fetching stations \(error)")
+            }
+        }
+    }
+    
+    func testFetchCopyright() {
+        Task {
+            do {
+                let client = Client(
+                    serverURL: try Servers.Server1.url(),
+                    transport: URLSessionTransport()
+                )
+                let service = CopyrightService(
+                    client: client,
+                    apikey: "e0940f60-7b86-40f1-ba94-6a70f7d38166"
+                )
+                print("Fetching copyright...")
+                let copyright = try await service.getCopyright(
+                    format: "json"
+                )
+                print("Successfully fetched copyright \(copyright)")
+            } catch {
+                print("Error fetching copyright \(error)")
+            }
+        }
     }
 }
 
