998235bf61de502b864165cd8568f432cf5c93b0
kristinatsk <kristinatsk@gmail.com>
2026-07-05 00:56:57 +0300

    feat: add UserAgreement screen

--- TravelSchedule/Views/SettingsView.swift
+++ TravelSchedule/Views/SettingsView.swift
@@ -4,39 +4,41 @@ struct SettingsView: View {
     @AppStorage(Constants.Storage.isDarkMode) var isDarkMode = false
     
     var body: some View {
-        VStack {
-            List {
-                Toggle("Темная тема", isOn: $isDarkMode)
-                    .tint(.blueUniversal)
-                    .padding(.vertical, 9)
-                ZStack {
-                    HStack {
-                        Text("Пользовательское соглашение")
-                        Spacer()
-                        Image(systemName: Constants.Icons.chevronRight)
-                            .foregroundColor(.primary)
+        NavigationStack {
+            VStack {
+                List {
+                    Toggle("Темная тема", isOn: $isDarkMode)
+                        .tint(.blueUniversal)
+                        .padding(.vertical, 9)
+                    ZStack {
+                        HStack {
+                            Text("Пользовательское соглашение")
+                            Spacer()
+                            Image(systemName: Constants.Icons.chevronRight)
+                                .foregroundColor(.primary)
+                        }
+                        NavigationLink {
+                            UserAgreementView()
+                        } label: {
+                            Color.clear
+                        }
+                        .opacity(0)
                     }
-                    NavigationLink {
-                        UserAgreementView()
-                    } label: {
-                        Color.clear
-                    }
-                    .opacity(0)
+                    .listRowSeparator(.hidden)
                 }
-                .listRowSeparator(.hidden)
-            }
-            .listStyle(.plain)
-            .padding(.top, 19)
-            
-            VStack(spacing: 16) {
-                Text("Приложение использует API «Яндекс.Расписания»")
-                    .foregroundColor(.primary)
-                Text("Версия 1.0 (beta)")
-            }
-            .font(.system(size: 12, weight: .regular))
-            .foregroundColor(.primary)
-            .padding(.bottom, 24)
+                .listStyle(.plain)
+                .padding(.top, 19)
+                
+                VStack(spacing: 16) {
+                    Text("Приложение использует API «Яндекс.Расписания»")
+                        .foregroundColor(.primary)
+                    Text("Версия 1.0 (beta)")
+                }
+                .font(.system(size: 12, weight: .regular))
+                .foregroundColor(.primary)
+                .padding(.bottom, 24)
 
+            }
         }
     }
 }
--- TravelSchedule/Views/UserAgreementView.swift
+++ TravelSchedule/Views/UserAgreementView.swift
@@ -1,8 +1,32 @@
 import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        
+        let urlRequest = URLRequest(url: url)
+        webView.load(urlRequest)
+        
+        return webView
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        
+    }
+    
+}
 
 struct UserAgreementView: View {
-    var body: some View {
-        Text("UserAgreement")
+        var body: some View {
+            VStack {
+                if let agreementURL = URL(string: "https://yandex.ru/legal/practicum_offer") {
+                    WebView(url: agreementURL)
+                }
+            }
+            .navigationTitle("Пользовательское соглашение")
     }
 }
 
